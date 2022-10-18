a21030fe8ce99c3bd04cd4c7ca5515ae530c0987
jesmin-juthi <thesathil67@gmail.com>
2022-10-18 14:05:53 +0600

    Double the entered number

---
+++ double.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int a, doubled;
+    printf("Enter a positive integer number ");
+    scanf("%d",&a);
+
+    doubled = a * 2;
+
+    printf("Double of the number you have entered is %d", doubled);
+
+    return 0;
+}
